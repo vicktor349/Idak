623f631ffc8652a0a4b75677e20eceda70a136f8
vicktoh <ayomidevictor349@gmail.com>
2024-04-25 11:21:28 +0100

    Featured category data

---
+++ data/featuredlistingdata.js
@@ -0,0 +1,58 @@
+const featuredlistingdata = [
+    {
+        id: "1",
+        imageurl: "/featuredlisting/luxuryhome.png",
+        pricing: "85,000",
+        location: "Luxury Villa in Rego Park",
+        bedrooms: "3",
+        size: "2,500",
+        seller: "Anne Liza"
+    },
+    {
+        id: "2",
+        imageurl: "/featuredlisting/luxuryhouse.png",
+        pricing: "105,000",
+        location: "Luxury Villa in Rego Park",
+        bedrooms: "5",
+        size: "2,500",
+        seller: "Anne Liza"
+    },
+    {
+        id: "3",
+        imageurl: "/featuredlisting/luxuryvilla.png",
+        pricing: "200,560",
+        location: "Luxury Villa in Rego Park",
+        bedrooms: "10",
+        size: "10,000",
+        seller: "Anne Liza"
+    },
+    {
+        id: "4",
+        imageurl: "/featuredlisting/luxuryhome.png",
+        pricing: "85,000",
+        location: "Luxury Villa in Rego Park",
+        bedrooms: "3",
+        size: "2500",
+        seller: "Anne Liza"
+    },
+    {
+        id: "5",
+        imageurl: "/featuredlisting/luxuryhouse.png",
+        pricing: "92,000",
+        location: "Luxury Villa in Rego Park",
+        bedrooms: "4",
+        size: "4500",
+        seller: "Anne Liza"
+    },
+    {
+        id: "6",
+        imageurl: "/featuredlisting/luxuryhome.png",
+        pricing: "85,000",
+        location: "Luxury Villa in Rego Park",
+        bedrooms: "3",
+        size: "2500",
+        seller: "Anne Liza"
+    }
+]
+
+export default featuredlistingdata
