098ed7c9a598e38f4ecc770b1a4e213fd1d7cc6a
vicktoh <ayomidevictor349@gmail.com>
2024-05-02 15:00:20 +0100

    location data

---
+++ data/featuredlocationdata.js
@@ -0,0 +1,25 @@
+const featuredlocation = [
+    {
+        id: "1",
+        imageUrl: "/featuredlocation/Anuradhapura.png",
+        name: "Anuradhapura"
+    },
+    {
+        id: "2",
+        imageUrl: "/featuredlocation/Gampaha.png",
+        name: "Gampaha"
+    },
+    {
+        id: "3",
+        imageUrl: "/featuredlocation/Badulla.png",
+        name: "Badulla"
+    },
+    {
+        id: "4",
+        imageUrl: "/featuredlocation/Jaffna.png",
+        name: "Jaffna"
+    }
+
+]
+
+export default featuredlocation;
