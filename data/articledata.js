1b83eed8daacc64ace8e1998019a62a1f9ad80a4
vicktoh <ayomidevictor349@gmail.com>
2024-05-03 16:01:57 +0100

    article data

---
+++ data/articledata.js
@@ -0,0 +1,32 @@
+const articledata = [
+    {
+        id: "1",
+        date: "May 7, 2023",
+        imageUrl: "/articles/rental.png",
+        title: "Car Rental Hacks for a Smooth Journey",
+        text: "Learn insider tips for getting the best deals and making the most of your rental car experience."
+    },
+    {
+        id: "2",
+        date: "April 25, 2023",
+        imageUrl: "/articles/managing.png",
+        title: "Managing Your Rental Items: A Comprehensive Guide",
+        text: "Get valuable insights on how to effectively manage and maintain your rented items."
+    },
+    {
+        id: "3",
+        date: "January 2, 2023",
+        imageUrl: "/articles/navigating.png",
+        title: "Navigating the Rental Market: Insights for New Renters",
+        text: "Explore key considerations and useful tips for individuals new to the rental market."
+    },
+    {
+        id: "4",
+        date: "April 7, 2023",
+        imageUrl: "/articles/maximizing.png",
+        title: "Maximizing Returns: Strategies for Rental Property Owners",
+        text: "Learn strategies to optimize the return on investment for your rental property."
+    },
+]
+
+export default articledata;
