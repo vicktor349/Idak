48e7f04dc54fe73105b6b3b97b0c6cbf97cc35e8
vicktoh <ayomidevictor349@gmail.com>
2024-05-03 16:10:48 +0100

    Footer component

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () =>
+{
+    return (
+        <div className='bg-gray-200 w-full h-48 flex items-center justify-center'>
+            <p> © 2020 Idak All rights reserved.</p>
+        </div>
+    )
+}
+
+export default Footer
